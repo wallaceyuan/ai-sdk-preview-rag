38663f53fccc07c204b98f37a9a8809c68737a38
wallaceyuan <1272864289@qq.com>
2024-09-26 14:46:17 +0800

    feat: add proxy

--- app/(preview)/api/chat/route.ts
+++ app/(preview)/api/chat/route.ts
@@ -1,6 +1,7 @@
 import { createResource } from "@/lib/actions/resources";
 import { findRelevantContent } from "@/lib/ai/embedding";
-import { openai } from "@ai-sdk/openai";
+import fetch from '@/lib/fetch'
+import { createOpenAI } from "@ai-sdk/openai";
 import { convertToCoreMessages, generateObject, streamText, tool } from "ai";
 import { z } from "zod";
 
@@ -9,6 +10,12 @@ export const maxDuration = 30;
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
+
+  const openai = createOpenAI({
+    // custom settings, e.g.
+    // compatibility: 'strict', // strict mode, enable when using the OpenAI API
+    fetch: fetch
+  });
 
   const result = await streamText({
     model: openai("gpt-4o"),
